a9cfd170401cd149ed0bf6769fbe1eeefb4cdaca
Alex Fish <fish@ustwo.co.uk>
2012-10-30 15:49:45 +0000

    Adding more specs
    Adding job spec
    Adding jenkins spec

---
+++ spec/lib/US2/jenkins_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe US2::Jenkins, "-" do
+  
+  it "needs tests"
+  
+end
---
+++ spec/models/job_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe Job, "-" do
+   
+  it "needs tests"
+  
+end 
