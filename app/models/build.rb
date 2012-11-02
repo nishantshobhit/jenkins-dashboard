aba768156b935ad6b0d2f2666d667e49a8648b30
Alex Fish <fish@ustwo.co.uk>
2012-11-02 17:56:56 +0000

    Fixing concurency test-report issue

--- app/models/build.rb
+++ app/models/build.rb
@@ -2,13 +2,13 @@ class Build < ActiveRecord::Base
   attr_accessible :duration, :name, :number, :success, :url
   validates_presence_of :name, :number, :duration
   after_save :update_culprits, :get_test_report
-  
+
   has_and_belongs_to_many :culprits
   belongs_to :job
   has_one :test_report
-  
+
   class << self
-    
+
     def from_api_response(api_response,job)
       #set up vars
       duration = api_response["duration"]
@@ -29,33 +29,34 @@ class Build < ActiveRecord::Base
       #return build
       @build
     end
-    
+
   end
 
   def is_in_database
     @query = Build.find(:all, :conditions => {:number => self.number, :name => self.name})
     @query.any?
   end
-  
+
   def update_culprits
     # increment the culprits count
     self.increment_culprits_count unless self.culprits.length == 0 or self.success
   end
-  
+
   def increment_culprits_count
     self.culprits.each do |culprit|
       count = culprit.count + 1
       culprit.update_attributes(:count => count)
     end
   end
-  
+
   def get_test_report
-    US2::Jenkins.instance.get_test_report(self) do |report|
+    jenkins = US2::Jenkins.new()
+    jenkins.get_test_report(self) do |report|
       report.build = self unless report.nil?
       report.save! unless report.nil?
      end
   end
-  
+
   def health_response
     {:created_at => self.created_at, :success => self.success}
   end
