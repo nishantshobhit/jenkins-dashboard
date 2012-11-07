e046b923e9142322387e6075749a6f27c148a2d3
Alex Fish <fish@ustwo.co.uk>
2012-11-07 15:32:42 +0000

    Updating duration to only return succesfull builds

--- app/controllers/api/duration_controller.rb
+++ app/controllers/api/duration_controller.rb
@@ -3,7 +3,7 @@ module Api
   class DurationController < BaseController
 
     def index
-      builds ||= Build.find(:all, :conditions => ["duration > 0 AND duration < 1000000"])
+      builds ||= Build.find(:all, :conditions => ["duration > 0 AND duration < 1000000 AND success == ?",true])
       respond_with(Build.duration_response_for_builds(builds))
     end
 
