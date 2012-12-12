0d984bf523d781723dac88b36a0caac893313e24
Alex Fish <fish@ustwo.co.uk>
2012-12-12 14:28:37 +0000

    Fixing bootstrap image path

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -3,6 +3,7 @@ class WidgetsController < ApplicationController
   def new
     @widget = Widget.new(:dashboard_id => params[:dashboard_id])
     @jobs = Job.find(:all)
+    @jobs.unshift(Job.new(:name => "All"))
   end
 
   def show
