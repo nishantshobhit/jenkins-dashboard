f525818abd4fd51569d1c77c953f6c30e2d90fe9
Alex Fish <fish@ustwo.co.uk>
2013-04-15 16:43:20 +0100

    Added Rails path specific reload check

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,6 +2,10 @@ module Api
 
   class BaseController < ApplicationController
     respond_to :json
+
+    def current_path
+      respond_with(Rails.root)
+    end
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   helper_method :current_user
+
+  def current_path
+    respond_with(Rails.root.to_json)
+  end
+
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  
+
   def require_user
     unless current_user
       redirect_to log_in_url
     return false
     end
   end
-  
+
 end
