3ba42af30b6381687139e38aa7b7b1edb3b63b73
Alex Fish <fish@ustwo.co.uk>
2013-04-18 09:26:18 +0100

    Updating sync method to use save!

--- lib/US2/Jenkins.rb
+++ lib/US2/Jenkins.rb
@@ -16,7 +16,7 @@ module US2
         job.save
         #get the latest build for the job and save it
         get_latest_build(job) do |build|
-          build.save
+          build.save!
         end
       end
     end
